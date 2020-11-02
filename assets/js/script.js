b39f0edbcb26e2c80c2953acb1a5934f02ecec45
Racheal Colbert <rachealcolbert16@gmail.com>
2020-11-01 17:20:16 -0700

    submit add event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var weatherFormEl = document.querySelector("#weather-search")
+var cityInputEl = document.querySelector("#city");
+
 var getDailyWeather = function (city) {
     var apiUrlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=fdd5c446e496fac706291ef838631708" + apiKey;
     fetch(apiUrlWeather)
@@ -18,5 +21,16 @@ var getDailyWeather = function (city) {
 var formCitySearch = function (event) {
     event.preventDefault();
     console.log(event);
-    var city = 
-}
+    var city = cityInputEl.value.trim();
+    if (city) {
+        getDailyWeather(city);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a valid city");
+    }
+    console.log(event);
+};
+
+weatherFormEl.addEventListener("submit", formCitySearch);
+
+var displayWeather
